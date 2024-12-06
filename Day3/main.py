6b052626ff66d94531c341a83044498dbd1d8395
Santosh.Divakaran <santosh.divakaran.07@gmail.com>
2024-12-06 23:29:16 +0530

    AOC: Day 3 part 2 Code

--- Day3/main.py
+++ Day3/main.py
@@ -33,5 +33,24 @@ def part1():
 
     print(sum)
 
+def part2():
+    part1 = 0
+    part2 = 0
+    enabled = True
+    for inst in re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", data):
+        match inst:
+            case "do()":
+                enabled = True
+            case "don't()":
+                enabled = False
+            case _:
+                x, y = map(int, inst[4:-1].split(","))
+                part1 += x * y
+                if enabled:
+                    part2 += x * y
+
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
+
 
 
