20e8e78639481b5b3f987fea8596f8c5af55390d
Santosh.Divakaran <santosh.divakaran.07@gmail.com>
2024-12-08 21:58:47 +0530

    AOC: Optimised Day 3 code

--- Day3/main.py
+++ Day3/main.py
@@ -13,12 +13,12 @@ def read_file():
 def part1():
     input = read_file()
     l = re.findall(r"mul\([^,a-z]+,[^)a-z]+\)", str(input))
-    print(l)
-    print(len(l))
+    # print(l)
+    # print(len(l))
 
     sum = 0
     for val in l:
-        print(val)
+        # print(val)
         a,b = val.split("(")[1].split(")")[0].split(",")
         try:
             if not int(a) or not int(b):
@@ -28,12 +28,37 @@ def part1():
             print("Sike", val)
             continue
 
-        print(a,b)
+        # print(a,b)
         sum = sum + int(a)*int(b)
 
-    print(sum)
+    print(f"Part 1: {sum}")
 
 def part2():
+    input = read_file()
+    l = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", str(input))
+    sum = 0
+    enabled = True
+    for val in l:
+        if val == "do()":
+            enabled = True
+        elif val == "don't()":
+            enabled = False
+
+        else:
+            if enabled == True:
+                a, b = val.split("(")[1].split(")")[0].split(",")
+                try:
+                    if not int(a) or not int(b):
+                        print("Sike", val)
+                        continue
+                except:
+                    print("Sike", val)
+                    continue
+
+                sum = sum + int(a) * int(b)
+    print(f"Part 2: {sum}")
+
+def aoc_day3_optimised():
     part1 = 0
     part2 = 0
     enabled = True
@@ -54,5 +79,8 @@ def part2():
     print(f"Part 1: {part1}")
     print(f"Part 2: {part2}")
 
-
+if __name__ == '__main__':
+    part1()
+    part2()
+    aoc_day3_optimised()
 
