7ef6a47be53b60a0d85c3bd5a3c81b8601c5f932
Santosh.Divakaran <santosh.divakaran.07@gmail.com>
2024-12-10 01:33:47 +0530

    AOC: Optimised Day 4 both part code

--- Day4/main.py
+++ Day4/main.py
@@ -1,35 +1,58 @@
 import os
 from collections import defaultdict
 from pathlib import Path
-import re
+
+
+def read_file():
+    rootpath = Path(__file__).parent
+    filepath = os.path.join(rootpath, 'input.txt')
+
+    with open(filepath, 'r') as file:
+        return file.readlines()
+
+
+lines = read_file()
+char_map = defaultdict(set)
+
+for r, row in enumerate(lines):
+    for c, val in enumerate(row):
+        char_map[val].add((r, c))
+
 
 def part1():
-    word_search = []
-    with open('input.txt', 'r') as f:
-        for line in f.readlines():
-            word_search.append(list(line.strip()))
-    n = len(word_search)
+    part1 = 0
+    for r, c in char_map["X"]:
+        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)]:
+            for i, char in enumerate("MAS", 1):
+                if (r + (dr * i), c + (dc * i)) not in char_map[char]:
+                    break
+            else:
+                part1 += 1
 
-    def is_inbounds(i, j):
-        return 0 <= i < n and 0 <= j < n
-
-    # part 1
-    result = 0
-    for i in range(n):
-        for j in range(n):
-            if word_search[i][j] != 'X':
-                continue
-            for di in [-1, 0, 1]:
-                for dj in [-1, 0, 1]:
-                    if (di, dj) == (0, 0):
-                        continue
-                    if is_inbounds(i + 3 * di, j + 3 * dj):
-                        if ''.join(word_search[i + k * di][j + k * dj] for k in range(4)) == 'XMAS':
-                            result += 1
-    print(result)
+    print(part1)
 
 
+def part2():
+    part2 = 0
+    upleft = lambda r, c: (r - 1, c - 1)
+    upright = lambda r, c: (r - 1, c + 1)
+    downleft = lambda r, c: (r + 1, c - 1)
+    downright = lambda r, c: (r + 1, c + 1)
+
+    for r, c in char_map["A"]:
+        if upleft(r, c) in char_map["M"]:
+            if upright(r, c) in char_map["M"] and downleft(r, c) in char_map["S"] and downright(r, c) in char_map["S"]:
+                part2 += 1
+            elif downleft(r, c) in char_map["M"] and upright(r, c) in char_map["S"] and downright(r, c) in char_map["S"]:
+                part2 += 1
+        elif downright(r, c) in char_map["M"]:
+            if upright(r, c) in char_map["M"] and downleft(r, c) in char_map["S"] and upleft(r, c) in char_map["S"]:
+                part2 +=1
+            elif downleft(r, c) in char_map["M"] and upright(r, c) in char_map["S"] and upleft(r, c) in char_map["S"]:
+                part2 +=1
+
+    print(part2)
 
 
 if __name__ == '__main__':
-    part1()
+    part2()
